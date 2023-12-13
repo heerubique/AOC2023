92b94808dca6579f051ec0e7bdf1725bb07a493a
Patrizia Heer <heer@ubique.ch>
2023-12-13 10:51:16 +0100

    AOC13

---
+++ AOC2023/AOC12.swift
@@ -0,0 +1,8 @@
+//
+//  AOC12.swift
+//  AOC2023
+//
+//  Created by Patrizia Heer on 12.12.2023.
+//
+
+import Foundation
---
+++ AOC2023/AOC13.swift
@@ -0,0 +1,8 @@
+//
+//  AOC13.swift
+//  AOC2023
+//
+//  Created by Patrizia Heer on 13.12.2023.
+//
+
+import Foundation
